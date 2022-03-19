789fdd3691b42b94dc426ac5ae6e6d5ab9b0581f
nickromanov <rno9292@gmail.com>
2022-03-20 00:01:11 +0300

    app created

---
+++ components/Tutorial.vue
@@ -0,0 +1,15 @@
+<!-- Please remove this file from your project -->
+<template>
+  <InputText type="text" />
+</template>
+
+<script>
+import InputText from 'primevue/inputtext';
+
+export default {
+  name: 'NuxtTutorial',
+  components: {
+    InputText
+  },
+}
+</script>
